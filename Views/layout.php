b835ae76d5acc216d944994f45ad1273e765b106
idekir <idekir@gmail.com>
2016-10-15 16:36:16 +0200

    css base

--- Views/layout.php
+++ Views/layout.php
@@ -1,24 +1,28 @@
-<?php ?>
 <!DOCTYPE html>
 <html>
+
     <head>
-        <link  rel="stylesheet" type="text/css" href="assets/css/style.css" />
+        <link  rel="stylesheet" type="text/css" href="style.css" />
         <title> <?php echo $title ?> </title>	
     </head>
-    <body>
-<?php
-if (isset($_SESSION['log'])) {
-    echo '<a id="boutonco" href="#">Deconnexion</a>';
-} else {
-    echo '<a id="boutonco" href="';
-    echo "index.php?page=connexion";
-    echo '"> Connexion </a>';
-}
-?>
-       
-            <div id="navbar">
 
-                <a href="index.php"> Accueil </a>
+    <body>
+        <?php
+        if (isset($_SESSION['log'])) {
+            echo '<a class="boutonco" href="#">Deconnexion</a>';
+        } else {
+            echo '<a class="boutonco" href="';
+            echo "index.php?page=connexion";
+            echo '"> Connexion </a>';
+        }
+        ?>
+
+        <div class="header">header</div>
+
+        <div class="navBar">
+            <div class="navLeft">A.M.E</div>
+            <div class="navMenu">
+                <a href="index.php">Accueil</a>
                 <a href="index.php?page=discipline">La Discipline</a>
                 <a href="index.php?page=clubs"> Les Clubs </a>
                 <a href="index.php?page=programme"> Le Programme</a>
@@ -26,21 +30,23 @@ if (isset($_SESSION['log'])) {
                 <a href="index.php?page=photo"> Albums Photo</a>
                 <a href="index.php?page=liens"> Liens Utiles </a>
                 <a href="index.php?page=discipline"> Nous contacter</a>
+            </div>
+            <div class="navRight">
                 <div id="searchbar">
-                    <form name="search" method="POST">
-                        <label>Rechercher<input type="text"/></label>
-
-                        <button name="search"> ok </button>
+                    <form id="formSearch" name="search" method="POST">
+                        <input id="recherche" type="text"/><button id ="search" name="search">R</button>
                     </form>
                 </div>
             </div>
-     
+        </div>
 
-<div id="content">
-        <div id="left-side">  image du coté</div> 
-        <div id="middle"><?php echo $content ?></div>
-        <div id="right-side"> image du coté </div>
-</div>
+        <div class="main">
+            <div class="left-side">  image cote gauche</div>
+            <div class="content">content<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
+            <div class="right-side"> image cote droit</div>
+        </div>
+
+        <div class="footer">footer<br><br></div>
 
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,55 +1,46 @@
 <?php
 
-if(isset($_GET['page']))
-{
-	switch($_GET['page'])
-	{
-		case 'creation' :
-						
-						include('Views/creation.php');
-						break;
-		
-		case 'connexion':
-						include('Views/connexion.php');
-						break;
-		
-		case 'clubs' : 
-						include('Views/clubs.php');
-						break;
-						
-		case 'contact': 
-						include('Views/contact.php');
-						break;
-						
-		case 'discipline':
-		
-						include('Views/discipline.php');
-						break;
-						
-		case 'events' : 
-						include('Views/events.php');	
-						break;
-						
-		case 'liens'  : 
-						include('Views/liens.php');
-						break;
-						
-		case 'programme' :
-						include('Views/programme.php');
-						break;
-						
-		case 'albums' : 
-						include('Views/albums.php');
-						break;
-						
-	}
-	
-	
+if (isset($_GET['page'])) {
+    switch ($_GET['page']) {
+        case 'creation' :
+
+            include('Views/creation.php');
+            break;
+
+        case 'connexion':
+            include('Views/connexion.php');
+            break;
+
+        case 'clubs' :
+            include('Views/clubs.php');
+            break;
+
+        case 'contact':
+            include('Views/contact.php');
+            break;
+
+        case 'discipline':
+
+            include('Views/discipline.php');
+            break;
+
+        case 'events' :
+            include('Views/events.php');
+            break;
+
+        case 'liens' :
+            include('Views/liens.php');
+            break;
+
+        case 'programme' :
+            include('Views/programme.php');
+            break;
+
+        case 'albums' :
+            include('Views/albums.php');
+            break;
+    }
+} else {
+    include('Views/accueil.php');
 }
-else
-	{
-		include('Views/accueil.php');
-	}
-	
-	
-	?>
+?>
