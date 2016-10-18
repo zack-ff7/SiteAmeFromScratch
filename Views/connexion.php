849f2ad0a90aee16cbefa2c109e265e41c177220
idekir <idekir@gmail.com>
2016-10-18 11:28:29 +0200

    Style connection

--- Views/connexion.php
+++ Views/connexion.php
@@ -5,28 +5,30 @@ ob_start();
 
 <div id="connection">
 
-<form method="POST">
-    <label>Login<input type="text" name="login"/></label>
-    <label>Mot de passe<input type="password" name="lepass"/></label>
-    <button type="submit" name="connect"> Se connecter</button>
-</form>
+    <div id="connectionPanel">
+        <form method="POST">
+            <label>Login</label>
+            <input type="text" name="login"/>
+            <label>Mot de passe</label>
+            <input type="password" name="lepass"/>
+            <button type="submit" name="connect">Se connecter</button>
+        </form>
+        <div id="connectionPanelNew"><p>Pas encore de compte? Cliquez<a href="index.php?page=creation"> ici</a></div>
     </div>
-    
-<p>Pas encore de compte? Cliquez <a href="index.php?page=creation"> ici </a>
-  
- <?php
+</div>
 
- if(isset($Message))
- {
-     echo '<p>'.$Message.'</p>';
- }
- 
+
+<?php
+if (isset($Message)) {
+    echo '<p>' . $Message . '</p>';
+}
+
 
 
 $content = ob_get_clean();
 include("Views/layout.php");
 ?>
 
- 
+
 
 
