0d34729b69be61cd57939db6a6baaa0945d0b6df
Armand <armand.toquet@etu.univ-lyon1.fr>
2016-10-18 10:53:02 +0200

    erreur
    erreur

---
+++ Views/gestion_membres.php
@@ -0,0 +1,13 @@
+<?php
+$title='Gestion des membres';
+ob_start();
+
+?>
+
+
+
+<?php
+$content=  ob_get_clean();
+include ('Views/layout.php');
+?>
+
--- Views/layout.php
+++ Views/layout.php
@@ -40,9 +40,7 @@
                     <a href="index.php?page=clubs">Les Clubs</a>
                     <a href="index.php?page=programme">Le Programme</a>
                     <a href="index.php?page=events">Evenements</a>
-                    <a href="index.php?page=album">Albums Photo</a>';
-
-
+                    <a href="index.php?page=album">Albums Photo</a>
                     <a href="index.php?page=liens">Liens Utiles</a>
                     <a href="index.php?page=discipline">Contact</a>
                 </div>
