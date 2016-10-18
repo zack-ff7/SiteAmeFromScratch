27779e049541352dafdae108c5f865218ab65ec1
Armand <armand.toquet@etu.univ-lyon1.fr>
2016-10-18 11:10:00 +0200

    new
    new

--- Views/deconnexion.php
+++ Views/deconnexion.php
@@ -5,9 +5,10 @@ ob_start();
 ?>
 <p>Vous allez être redirigé sur l'accueil dans 5 secondes.<p>
 <?php
-$content=  ob_get_clean();
+
 
 session_destroy();
+$content=  ob_get_clean();
 include('Views/layout.php');
 sleep(5);
 header('Location: index.php');
