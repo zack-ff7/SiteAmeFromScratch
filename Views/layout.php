74814d89117a068d2154467b2e8a1953a353c533
idekir <idekir@gmail.com>
2016-11-01 12:17:38 +0100

    ajout symbole

--- Views/layout.php
+++ Views/layout.php
@@ -31,7 +31,7 @@
         </div>
 
         <div class="navBar">
-            <div class="navLeft"><h1>A.M.E</h1></div>
+            <div class="navLeft"><img src="assets/images/symbole.png" alt="symbole"><h1>A.M.E</h1></div>
             <div class="navMenu">
                 <a href="index.php">Accueil</a>
                 <a href="index.php?page=discipline">La Discipline</a>
