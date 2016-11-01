479dad81cf17bc396f6704cdd259de507dd78061
Armand <armand.toquet@etu.univ-lyon1.fr>
2016-11-01 15:04:06 +0100

    Revert "com"
    This reverts commit 8937f62d445b5ea1b001e33f6dcabe5948042964.

--- Views/albums.php
+++ Views/albums.php
@@ -2,29 +2,9 @@
 $title="Les albums";
 ob_start();
 
-if(isset($_SESSION['Statut']))
+if(isset($_SESSION['Statut'])==0 || isset($_SESSION['Statut'])==1)
 {
     echo 'Voici les albums photos';
-    if(isset($_SESSION['log']))
-    {
-        foreach ($lesalbums as $ligne)
-        {
-            echo $ligne['NomAlbum'];
-        }
-        echo '<form method="POST" >';
-        echo '<label>Rajouter un Album</label>';
-        echo '<input type="text" name="lenom"/>';
-        echo '<input type="submit" name"envoyerAlb"/>';
-        echo '</form>';
-        
-        if(isset($message))
-        {
-            echo '<p>'.$message.'</p>';
-        }
-        
-        
-    }
-    
 }
 else
 {
