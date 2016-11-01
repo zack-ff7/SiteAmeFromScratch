54d081b98950ad6133b574cbb2dad9f97c47a602
idekir <idekir@gmail.com>
2016-11-01 16:31:26 +0100

    ajout ckeditor pour liens et programmes

--- Views/liens.php
+++ Views/liens.php
@@ -1,5 +1,27 @@
 <?php
-$title="Liens Utiles";
-$content="Les liens utiles sont ici";
+$title = 'Liens utiles';
+ob_start();
+?>
+<div id="liens">
+
+    <?php
+    if (isset($_SESSION['log']) && $_SESSION['Statut'] == 1) {
+        ?>
+        <textarea name="editorLiens" id="editorLiens">texte a charger/sauvegarder</textarea>
+        <script>CKEDITOR.replace('editorLiens');</script>
+        <?php
+    } else {
+        ?>
+        <textarea name="editorLiens" id="editorLiens">texte a charger/sauvegarder</textarea>
+        <?php
+    }
+    ?>
+</div>
+    <?php
+    $content = ob_get_contents();
+    ob_end_clean();
+    ?>
+
+<?php
 include("Views/layout.php");
 ?>
--- Views/programme.php
+++ Views/programme.php
@@ -1,6 +1,27 @@
 <?php
-$title="Le programme";
-$content="Les programmes du club";
+$title = 'Le Prrogramme';
+ob_start();
+?>
+<div id="programme">
+
+    <?php
+    if (isset($_SESSION['log']) && $_SESSION['Statut'] == 1) {
+        ?>
+        <textarea name="editorProgramme" id="editorProgramme">texte a charger/sauvegarder</textarea>
+        <script>CKEDITOR.replace('editorProgramme');</script>
+        <?php
+    } else {
+        ?>
+        <textarea name="editorProgramme" id="editorProgramme">texte a charger/sauvegarder</textarea>
+        <?php
+    }
+    ?>
+</div>
+    <?php
+    $content = ob_get_contents();
+    ob_end_clean();
+    ?>
+
+<?php
 include("Views/layout.php");
 ?>
-
