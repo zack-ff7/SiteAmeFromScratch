5945b8afadd85371848b3255b4ad68f7a2d7e31f
idekir <idekir@gmail.com>
2016-11-01 14:42:08 +0100

    ajout ckeditor discipline

--- Views/discipline.php
+++ Views/discipline.php
@@ -1,11 +1,18 @@
 <?php
-$title='Presentation discipline';
-$content='La Discipline';
+
+$title = 'La Discipline';
+ob_start();
 ?>
 
-
-
+<textarea name="editorDiscipline" id="editorDiscipline">ckeditor</textarea>
+<script>CKEDITOR.replace('editorDiscipline');</script>
 
 <?php
+$content = ob_get_contents();
+ob_end_clean();
+?>
+
+<?php
+
 include("Views/layout.php");
 ?>
