6cc6282245cd1c6ce3471bc9d4d3d20d2cc5fb1a
idekir <idekir@gmail.com>
2016-11-01 14:33:51 +0100

    pti commit

--- Views/clubs.php
+++ Views/clubs.php
@@ -1,20 +1,28 @@
 <?php
-
 $title = "Les Clubs";
 ob_start();
-if (isset($_SESSION['log']) && $_SESSION['Statut'] == 1) {
+?>
 
-    echo '<div id="form_club"><form method="POST">';
-    echo '<label>Nom du nouveau Club<input type="text" name="nom"/>';
-    echo '<input type="submit" value="Valider" name="valider"/>';
-    echo '</form></div>';
-}
+<div id="lesClubs">
+
+    <?php
+    if (isset($_SESSION['log']) && $_SESSION['Statut'] == 1) {
+
+        echo '<div id="form_club"><form method="POST">';
+        echo '<label>Nom du nouveau Club<input type="text" name="nom"/>';
+        echo '<input type="submit" value="Valider" name="valider"/>';
+        echo '</form></div>';
+    }
 
 
-foreach ($data as $ligne) {
-    echo '<div><a href="index.php?page=clubs&club='. $ligne['nom_club'] . '">' . $ligne['nom_club'] . '</a></div>';
-}
+    foreach ($data as $ligne) {
+        echo '<a class="clubs" href="index.php?page=clubs&club=' . $ligne['nom_club'] . '">' . $ligne['nom_club'] . '</a>';
+    }
+    ?>
 
+</div>
+
+<?php
 $content = ob_get_clean();
 include("Views/layout.php");
 ?>
